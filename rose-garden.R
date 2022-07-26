519397568c21f5d65b39480b5451c260c13da3f4
robbrotherton <robert.brotherton@gmail.com>
2022-07-26 08:54:39 -0400

    update colors

--- rose-garden.R
+++ rose-garden.R
@@ -2,26 +2,30 @@
 
 library(tidyverse)
 
-# helper function
+# helper function to pick random rose parameters
 rand <- function() round(runif(1, 1, 20))
-# rand <- function() sample(18, 1)
 
-# generate a star from given paramaters
-starr <- function(a, b, c, n = 200, coils = 20, expansion = 10, spiral_b = .01) {
+# generate a star from given parameters
+starr <- function(a, b, c,
+                  n = 200,
+                  coils = 20,
+                  expansion = 10) {
 
-  theta <- seq(0,2*pi*coils, length.out = n*coils)
+  points <- n * coils
 
-  spiral_radius <- theta * seq(1, expansion, length.out = n*coils)
+  theta <- seq(0,2*pi*coils, length.out = points)
 
-  rose <- tibble(x = numeric(n*coils),
-                 y = numeric(n*coils))
+  spiral_radius <- theta * seq(1, expansion, length.out = points)
+
+  rose <- tibble(x = numeric(points),
+                 y = numeric(points))
 
   for(i in 1:length(theta)) {
 
     t <- theta[i] %% (pi*2)
 
-    one <- 2+sin(a*t)/2
-    two <- t+sin(b*t)/c
+    one <- 2 + sin(a * t) / 2
+    two <- t + sin(b * t) / c
 
     rose$x[i] <- spiral_radius[i] * one * cos(two)
     rose$y[i] <- spiral_radius[i] * one * sin(two)
@@ -32,28 +36,28 @@ starr <- function(a, b, c, n = 200, coils = 20, expansion = 10, spiral_b = .01) 
 
 }
 
-# eg <- starr(6, 18, 18, coils = 50, expansion = 1)
-# ggplot(eg) + geom_path(aes(x, y))
+# eg <- starr(3, 18, 20, coils = 50, expansion = 1)
+# ggplot(eg) + geom_path(aes(x, y)) + coord_fixed()
 
-# parameters for rose grid
-
+# parameters for grid
 w <- 4
 h <- 5
-points <- 200
-coils <- 20
+n <- 200
+c <- 20
 
 
 palette <- c('#7689a6', '#ff6381', '#a0a776', '#f8ca76')
 # palette <- c('#d00001', '#ac0000', '#d3635f', '#b91556') # reds
+
 set.seed(3)
 
-rose_grid <- map_df(.x = 1:(w*h),
-                    .f = ~starr(a = rand(), b = rand(), c = rand()),
+rose_grid <- map_df(.x = 1:(w * h),
+                    .f = ~starr(a = rand(), b = rand(), c = rand(),
+                                n = n, coils = c),
                     .id = "group") |>
-  mutate(group2 = rep(1:(w*h), each = coils*points),
-         color = rep(sample(length(palette), w*h, replace = TRUE), each = coils*points),
-         xoff = rep(1:w*7000, each = coils*points, length.out = n()),
-         yoff = rep(1:h*7000, each = coils*points*w, length.out = n()),
+  mutate(color = rep(sample(length(palette), w * h, replace = TRUE), each = c * n),
+         xoff = rep(1:w * 7000, each = c * n, length.out = c * n * w * h),
+         yoff = rep(1:h * 7000, each = c * n * w, length.out = c * n * w * h),
          x0 = x + xoff,
          y0 = y + yoff) %>%
   select(x = x0, y = y0, group, color)
@@ -62,12 +66,12 @@ rose_grid <- map_df(.x = 1:(w*h),
 
 ggplot(rose_grid, aes(x, y, group = group,
                       color = factor(color))) +
-  geom_path() +
+  geom_path(size = .3) +
   scale_color_manual(values = palette) +
   coord_fixed() +
   theme_void() +
   theme(legend.position = 'none',
-        panel.background = element_rect(fill = hsv(.25, .05, .95))) +
-  NULL
+        panel.background = element_rect(fill = hsv(.25, .03, .97),
+                                        color = NA))
 
-ggsave("rose-garden.png", width = 8, height = 10)
+ggsave("rose-garden.png", width = 5, height = 7)
