fa01c89bcc8bc37c049a5d9c7996e835a5913dda
robbrotherton <robert.brotherton@gmail.com>
2022-07-26 08:41:15 -0400

    add color

--- rose-garden.R
+++ rose-garden.R
@@ -1,19 +1,17 @@
+# see https://mathworld.wolfram.com/StarrRose.html
+
 library(tidyverse)
 
+# helper function
+rand <- function() round(runif(1, 1, 20))
+# rand <- function() sample(18, 1)
 
-
-rand <- function() round(runif(1, 0, 20))
-
-
-starr <- function(a, b, c, n, coils, expansion = 10, spiral_b = .01) {
+# generate a star from given paramaters
+starr <- function(a, b, c, n = 200, coils = 20, expansion = 10, spiral_b = .01) {
 
   theta <- seq(0,2*pi*coils, length.out = n*coils)
-  # spiral_r <- exp(spiral_b*theta)
-  spiral_r <- theta * seq(1, expansion, length.out = n*coils)
-  # return(spiral_r)
-  # a <- 10
-  # b <- 1
-  # r <- a + b * theta
+
+  spiral_radius <- theta * seq(1, expansion, length.out = n*coils)
 
   rose <- tibble(x = numeric(n*coils),
                  y = numeric(n*coils))
@@ -21,54 +19,55 @@ starr <- function(a, b, c, n, coils, expansion = 10, spiral_b = .01) {
   for(i in 1:length(theta)) {
 
     t <- theta[i] %% (pi*2)
-    # r <- sin(n*k)
+
     one <- 2+sin(a*t)/2
     two <- t+sin(b*t)/c
 
-    # r <- 1-seq(1, .1, length.out = n)^.5
-    # r <- sqrt(seq(1, 0, length.out = n))
-    # r <- seq(1, 0, length.out = n)
-    # spiral_r <- cos(theta[i])*theta[i]
-    # rose$x[i] <- one * cos(two) * r
-    # rose$y[i] <- one * sin(two) * r
-
-    # rose$x[i] <- theta[i] * one * cos(two)
-    # rose$y[i] <- theta[i] * one * sin(two)
-
-    rose$x[i] <- spiral_r[i] * one * cos(two)
-    rose$y[i] <- spiral_r[i] * one * sin(two)
+    rose$x[i] <- spiral_radius[i] * one * cos(two)
+    rose$y[i] <- spiral_radius[i] * one * sin(two)
 
   }
 
-  return(rose)
+  rose
 
 }
 
+# eg <- starr(6, 18, 18, coils = 50, expansion = 1)
+# ggplot(eg) + geom_path(aes(x, y))
 
-points <- 200
-coils <- 20
+# parameters for rose grid
+
 w <- 4
 h <- 5
+points <- 200
+coils <- 20
+
+
+palette <- c('#7689a6', '#ff6381', '#a0a776', '#f8ca76')
+# palette <- c('#d00001', '#ac0000', '#d3635f', '#b91556') # reds
 set.seed(3)
 
 rose_grid <- map_df(.x = 1:(w*h),
-                    .f = ~starr(a = rand(), b = rand(), c = rand(),
-                                n = points, coils = coils, expansion = 10))
-
-rose_grid2 <- rose_grid %>%
-  mutate(group = rep(1:(w*h), each = coils*points),
+                    .f = ~starr(a = rand(), b = rand(), c = rand()),
+                    .id = "group") |>
+  mutate(group2 = rep(1:(w*h), each = coils*points),
+         color = rep(sample(length(palette), w*h, replace = TRUE), each = coils*points),
          xoff = rep(1:w*7000, each = coils*points, length.out = n()),
          yoff = rep(1:h*7000, each = coils*points*w, length.out = n()),
          x0 = x + xoff,
          y0 = y + yoff) %>%
-  select(x = x0, y = y0, group = group)
+  select(x = x0, y = y0, group, color)
 
 
 
-pic <- ggplot(rose_grid2, aes(x, y, group = group)) +
+ggplot(rose_grid, aes(x, y, group = group,
+                      color = factor(color))) +
   geom_path() +
+  scale_color_manual(values = palette) +
   coord_fixed() +
   theme_void() +
+  theme(legend.position = 'none',
+        panel.background = element_rect(fill = hsv(.25, .05, .95))) +
   NULL
 
 ggsave("rose-garden.png", width = 8, height = 10)
